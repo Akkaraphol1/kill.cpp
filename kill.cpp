e80ed21c5b896f1c4789b27170399ccbbac3193c
Akkaraphol1 <52001135+Akkaraphol1@users.noreply.github.com>
2019-07-03 09:44:52 +0700

    Add files via upload

---
+++ kill.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+int main () {
+	int x ;
+	cout << " Input Thai Baths : " ;
+	cin >> x ;
+	cout << "****ExchangeRate****" << endl ;
+	cout << x << " Baths = " << x*0.033 << " dollars" << endl ;
+	cout << x << " Baths = " << x*0.029 << " euros" << endl ;
+	cout << x << " Baths = " << x*0.028 << " Yen" << endl ;
+	cout << x << " Baths = " << x*0.0013  << "Dong" << endl ;
+	cout << x << " Baths = " << x*38.51 << " Ponds" << endl ;
+}
